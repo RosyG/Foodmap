c9774b11e7be842a67567c11347ec524dae76d0b
RosyG <rmga2106@gmail.com>
2018-01-18 22:02:22 -0600

    retos de código

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+  $("#food-1").mouseover (function () {
+    $("#text-1").fadeIn(500);
+  }).mouseout (function () {
+    $("#text-1").fadeOut(500);
+
+  });
+
+/*//Otra manera de escribir el código anterior.
+  $("#food-1").mouseover (show).mouseout (hidden);
+
+  function show () {
+    $("#text-1").fadeIn(500);
+  }
+  function hidden () {
+    $("#text-1").fadeOut(500);
+  }
+*/
+
+  $("#food-3").mouseover (function (){
+    $("#food-3").fadeToggle(5000);
+
+    $("#food-4").fadeIn(5000);
+    $("#food-4").fadeToggle(5000);
+    $("#food-4").remove();
+    $("#food-5").fadeIn(5000);
+  }).
+  $("#food-5").mouseout (function () {
+    /*$("#food-5").fadeOut(5000);*/
+    $("#food-4").fadeIn(5000);
+
+  });
