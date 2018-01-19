13be3b994251c8952efd65330e28528c6718ea59
RosyG <rmga2106@gmail.com>
2018-01-19 11:29:31 -0600

    añadiendo la función de splash screen

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
+function splash(time) {
+setTimeout(function () {
+ $('#splashscreen').hide();
+}, time);
+}
 
-  $("#food-1").mouseover (function () {
+  /*$("#food-1").mouseover (function () {
     $("#text-1").fadeIn(500);
   }).mouseout (function () {
     $("#text-1").fadeOut(500);
 
   });
 
-/*//Otra manera de escribir el código anterior.
+//Otra manera de escribir el código anterior.
   $("#food-1").mouseover (show).mouseout (hidden);
 
   function show () {
@@ -15,7 +20,7 @@
   function hidden () {
     $("#text-1").fadeOut(500);
   }
-*/
+
 
   $("#food-3").mouseover (function (){
     $("#food-3").fadeToggle(5000);
@@ -27,6 +32,8 @@
   }).
   $("#food-5").mouseout (function () {
     /*$("#food-5").fadeOut(5000);*/
+  /*
     $("#food-4").fadeIn(5000);
 
   });
+*/
