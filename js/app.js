1f0d21085665c72b0b6ca48284d1d842af5ca223
RosyG <rmga2106@gmail.com>
2018-01-19 19:19:51 -0600

    añadiendo imagenes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
-function splash(time) {
-setTimeout(function () {
- $('#splashscreen').hide();
-}, time);
+  function splash(time) {
+  setTimeout(function () {
+   $('#splashscreen').hide();
+  }, time);
+  }
+
+  $("#modal1").modal();
+
+
+/*
+$("#food-1").mouseover (function () {
+  $("#text-1").fadeIn(500);
+}).mouseout (function () {
+  $("#text-1").fadeOut(500);
+
+});
+
+//Otra manera de escribir el código anterior.
+$("#food-1").mouseover (show).mouseout (hidden);
+
+function show () {
+  $("#text-1").fadeIn(500);
 }
+function hidden () {
+  $("#text-1").fadeOut(500);
+}
+
+
+$("#food-3").mouseover (function (){
+  $("#food-3").fadeToggle(5000);
+
+  $("#food-4").fadeIn(5000);
+  $("#food-4").fadeToggle(5000);
+  $("#food-4").remove();
+  $("#food-5").fadeIn(5000);
+}).
+$("#food-5").mouseout (function () {
+  /*$("#food-5").fadeOut(5000);*/
+/*
+  $("#food-4").fadeIn(5000);
+
+});
 
   /*$("#food-1").mouseover (function () {
     $("#text-1").fadeIn(500);
@@ -37,3 +74,4 @@ setTimeout(function () {
 
   });
 */
+console.log(data[0].food);
---
+++ js/data.js
@@ -0,0 +1,35 @@
+//Data
+var data = [
+  {'name': 'The Capital Grille',
+   'adress':'Paseo de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+   'food': 'China'
+ },
+  {'name': 'Buena Barra',
+   'adress':'Paseo2 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+   'food': 'Italiana'},
+  {
+    'name': 'The Palm Santa Fe',
+     'adress':'Paseo3 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+     'food': 'Argentina'
+  },
+  {
+    'name': 'Bellini',
+     'adress':'Paseo4 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+     'food': 'Italiana'
+  },
+  {
+    'name': 'Restaurante Shu',
+     'adress':'Paseo5 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+     'food': 'Japonesa'
+  },
+  {
+    'name': 'Miralto',
+     'adress':'Paseo6 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+     'food': 'Portuguesa'
+  },
+  {
+    'name': 'Mexicanito',
+     'adress':'Paseo7 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+     'food': 'Mexicana'
+  }
+]
